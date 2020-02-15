2002e153246db797b36759f3e19999b2580184ee
Rotten-LKZ <Rotten_LKZ@163.com>
2020-02-15 17:46:41 +0800

    数量变化以及各地区数据图表

---
+++ 2019-nCov.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import requests
+import json
+from pyecharts import Bar
+
+header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36'}
+res = requests.get('http://www.dzyong.top:3005/yiqing/province',headers = header)
+res.encoding = 'utf-8'
+res = json.loads(res.text)
+
+provinceName = []
+confirmedNum = []
+curesNum = []
+deathsNum = []
+
+for e in res['data']:
+    provinceName.append(e['provinceName'])
+    confirmedNum.append(e['confirmedNum'])
+    curesNum.append(e['curesNum'])
+    deathsNum.append(e['deathsNum'])
+
+bar = Bar('各省市疫情数据','中国加油！')
+bar.add("确诊人数", provinceName, confirmedNum)
+bar.add("治愈人数", provinceName, curesNum)
+bar.add("死亡人数", provinceName, deathsNum)
+#is_convert=True为xy轴交换
+# bar.add("确诊人数", provinceName, confirmedNum,is_convert=True)
+# bar.add("治愈人数", provinceName, curesNum,is_convert=True)
+# bar.add("死亡人数", provinceName, deathsNum,is_convert=True)
+bar.show_config()
+bar.render("temperature.html")
---
+++ 2019-nCovHI.py
@@ -0,0 +1,40 @@
+
+#HI为history image
+import requests
+from pyecharts import Line
+import json
+
+header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36'}
+res = requests.get('http://www.dzyong.top:3005/yiqing/history',headers = header)
+res = json.loads(res.text)
+
+date = []
+confirmedNum = []
+suspectedNum = []
+curesNum = []
+deathsNum = []
+suspectedIncr = []
+
+for e in res['data']:
+    print(e)
+    date.append(e['date'])
+    confirmedNum.append(e['confirmedNum'])
+    suspectedNum.append(e['suspectedNum'])
+    curesNum.append(e['curesNum'])
+    deathsNum.append(e['deathsNum'])
+    suspectedIncr.append(e['suspectedIncr'])
+
+date.reverse()
+confirmedNum.reverse()
+suspectedNum.reverse()
+curesNum.reverse()
+deathsNum.reverse()
+
+line = Line('疫情趋势')
+line.add('确诊人数',date,confirmedNum,is_smooth=True)
+line.add('疑似人数',date,suspectedNum,is_smooth=True)
+line.add('治愈人数',date,curesNum,is_smooth=True)
+line.add('死亡人数',date,deathsNum,is_smooth=True)
+# line.add('')
+line.show_config()
+line.render()
